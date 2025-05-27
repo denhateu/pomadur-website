f6de5d321632a7710174233a55cca6923b61d53e
denhateu <danilaksenow101@gmail.com>
2025-05-27 13:34:44 +0500

    feat: added timer and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Box, Button, Heading, Text } from '@chakra-ui/react';
+import { Box, AbsoluteCenter, Text, Button } from '@chakra-ui/react';
 
 const App: React.FC = () => {
   return (
-    <Box p={5} textAlign="center">
-      <Heading mb={4}>Добро пожаловать в Chakra UI с Vite и TypeScript!</Heading>
-      <Text fontSize="lg" mb={4}>
-        Используйте готовые компоненты для быстрого создания интерфейсов.
-      </Text>
-      <Button colorScheme="teal">Кнопка</Button>
+    <Box position="relative" h="100vh">
+      <AbsoluteCenter w="full">
+        <Box w="full" maxW="300px">
+          <Text mb={4} textAlign="center" textStyle="6xl" fontWeight="bold">25:00</Text>
+          <Button w="full">Начать</Button>
+        </Box>
+      </AbsoluteCenter>
     </Box>
   );
 };
