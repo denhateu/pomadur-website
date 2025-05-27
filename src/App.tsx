c2af5db9cf5728e973aca3610f89db01d4dbd83f
denhateu <danilaksenow101@gmail.com>
2025-05-27 13:52:34 +0500

    chore: added header

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,19 @@ import { Container, Box, Text, Button } from '@chakra-ui/react';
 const App: React.FC = () => {
   return (
     <Box>
-      <Container>
-        <Box>
-          <Text mb={2} textAlign="center" textStyle="6xl" fontWeight="bold">25:00</Text>
-          <Button w="full">Начать</Button>
-        </Box>
-      </Container>
+      <Box py={4}>
+        <Container>
+          <Text textStyle="2xl" fontWeight="bold">Pomadur</Text>
+        </Container>
+      </Box>
+      <Box>
+        <Container>
+          <Box>
+            <Text mb={2} textAlign="center" textStyle="6xl" fontWeight="bold">25:00</Text>
+            <Button w="full">Начать</Button>
+          </Box>
+        </Container>
+      </Box>
     </Box>
   );
 };
