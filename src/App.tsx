c1577667fae075b457e9d08b73acfdb0f1b80480
denhateu <danilaksenow101@gmail.com>
2025-05-27 13:46:46 +0500

    chore: changed timer styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Box, AbsoluteCenter, Text, Button } from '@chakra-ui/react';
+import { Container, Box, Text, Button } from '@chakra-ui/react';
 
 const App: React.FC = () => {
   return (
-    <Box position="relative" h="100vh">
-      <AbsoluteCenter w="full">
-        <Box w="full" maxW="300px">
-          <Text mb={4} textAlign="center" textStyle="6xl" fontWeight="bold">25:00</Text>
+    <Box>
+      <Container>
+        <Box>
+          <Text mb={2} textAlign="center" textStyle="6xl" fontWeight="bold">25:00</Text>
           <Button w="full">Начать</Button>
         </Box>
-      </AbsoluteCenter>
+      </Container>
     </Box>
   );
 };
